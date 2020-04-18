bc12bcf7aa5e02f40a123f35904e740d8ae31304
YUCAVALCANTE <37241267+YUCAVALCANTE@users.noreply.github.com>
2020-04-18 01:16:52 -0300

    Add files via upload

---
+++ 001.3_exercicio_media.py
@@ -0,0 +1,9 @@
+#entrar com 2 números
+a = int(input("Digite o primeiro número: "))
+b = int(input("Digite o segundo número: "))
+
+# calcule a media
+media = (a+b) /2
+
+#imprimir a media
+print (media) 
