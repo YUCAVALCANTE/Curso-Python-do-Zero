d8913b2169291ae7d32a184e7197c3eca0915ab6
YUCAVALCANTE <37241267+YUCAVALCANTE@users.noreply.github.com>
2020-04-18 01:02:05 -0300

    Add files via upload

---
+++ 001.2_exercicio_area.py
@@ -0,0 +1,9 @@
+#ler raio (ponto flutuante)
+raio = float(input("Digite o raio do circulo: "))
+
+#calcular a area do circulo (area = pi * r * r ) ou (area = pi * r **2 )
+pi = 3.1415
+area = pi * raio ** 2
+
+#imprimir a area
+print (area)
