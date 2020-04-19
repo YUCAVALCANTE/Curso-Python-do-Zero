63f0eaa76250afe72f8386d469b1e941f0a8b560
Yury Cavalcante <37241267+YUCAVALCANTE@users.noreply.github.com>
2020-04-19 03:37:47 -0300

    Add files via upload

---
+++ 002.1_exercicio_Nota.py
@@ -0,0 +1,13 @@
+#receber nota do aluno
+nota = int(input("Digite a nota: "))
+if nota >= 60:
+
+# não esquecer a indentação
+    #se  (if) a nota for maior ou igual que 60
+    print("Aprovado")
+
+#se não 
+else:
+    #se a nota for menor que 60
+    print ("Reprovado")
+
